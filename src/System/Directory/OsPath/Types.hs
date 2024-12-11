ef1996859e29b13b81986c2433cca21fd4352185
Sergey Vinokurov <serg.foo@gmail.com>
2024-12-11 21:11:57 +0000

    NFData instances

--- src/System/Directory/OsPath/Types.hs
+++ src/System/Directory/OsPath/Types.hs
@@ -4,10 +4,11 @@
 -- License:    Apache-2.0 (see LICENSE)
 -- Maintainer: serg.foo@gmail.com
 
-{-# LANGUAGE DeriveFoldable    #-}
-{-# LANGUAGE DeriveFunctor     #-}
-{-# LANGUAGE DeriveGeneric     #-}
-{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DeriveFoldable             #-}
+{-# LANGUAGE DeriveFunctor              #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DeriveTraversable          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module System.Directory.OsPath.Types
   ( SymlinkType(..)
@@ -15,10 +16,13 @@ module System.Directory.OsPath.Types
   , Basename(..)
   ) where
 
+import Control.DeepSeq (NFData)
 import GHC.Generics (Generic, Generic1)
 
 data SymlinkType = Regular | Symlink
   deriving (Show, Read, Eq, Ord, Generic)
+
+instance NFData SymlinkType
 
 data FileType
   = File {-# UNPACK #-} !SymlinkType
@@ -26,7 +30,9 @@ data FileType
   | Other
   deriving (Show, Read, Eq, Ord, Generic)
 
+instance NFData FileType
+
 -- | Basename part of filename, without directory separators.
 newtype Basename a = Basename { unBasename :: a }
-  deriving (Eq, Ord, Show, Generic, Generic1, Functor, Foldable, Traversable)
+  deriving (Eq, Ord, Show, Generic, Generic1, NFData, Functor, Foldable, Traversable)
 
